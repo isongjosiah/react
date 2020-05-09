b838c264af230c5ce32151cf69a7baf75f70059c
isongjosiah <isongjosiah@gmail.com>
2020-05-09 21:40:35 +0100

    Add error handling and switch to axios from fetch

--- src/App.js
+++ src/App.js
@@ -21,6 +21,7 @@ class App extends Component {
       results: null,
       searchKey: '',
       searchTerm: DEFAULT_QUERY,
+      error: null,
     }
 
     this.needsToSearchTopStories = this.needsToSearchTopStories.bind(this)
@@ -57,7 +58,7 @@ class App extends Component {
     fetch(`${PATH_BASE}${PATH_SEARCH}?${PARAM_SEARCH}${searchTerm}&${PARAM_PAGE}${page}&${PARAM_HPP}${DEFAULT_HPP}`)
       .then(response => response.json())
       .then(result => this.setSearchTopStories(result))
-      .catch(error => error)
+      .catch(error => this.setState({ error }))
   }
 
   componentDidMount() {
@@ -87,16 +88,19 @@ class App extends Component {
     })
   }
   render() {
-    const { searchTerm, results, searchKey } = this.state
+    const { searchTerm, results, searchKey, error } = this.state
     const page = (results && results[searchKey] && results[searchKey].page) || 0
     const list = (results && results[searchKey] && results[searchKey].hits) || []
+
     return (
       <div className="page">
         <div className="interactions">
           <Search value={searchTerm} onChange={this.onSearchChange} onSubmit={this.onSearchSubmit} > Search </Search>
         </div>
-        {/* removed conditional rendering since we default to an empty list on line 83 if there is no result / if it is the first query */}
-        <Table list={list} onDismiss={this.onDismiss} />
+        {error
+          ? <div className="interactions">
+            <p>Something went wrong.</p>
+          </div> : < Table list={list} onDismiss={this.onDismiss} />}
         <div className="interactions">
           <Button onClick={() => this.fetchSearchTopStories(searchKey, page + 1)}> More </Button>
         </div>
