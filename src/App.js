ef7874ae53c7ddbc7a720746ae9edeef1566ca00
isongjosiah <isongjosiah@gmail.com>
2020-05-11 21:37:49 +0100

    react-create-app on Heroku

--- src/App.js
+++ src/App.js
@@ -24,19 +24,20 @@ const SORTS = {
   COMMENTS: list => sortBy(list, 'num_comments').reverse(),
   POINTS: list => sortBy(list, 'points').reverse()
 }
+
 const updateSearchTopStoriesState = (hits, page) => (prevState) => {
-  const { searchKey, results } = this.state
+  const { searchKey, results } = prevState
 
   const oldHits = results && results[searchKey] ? results[searchKey].hits : []
 
   const updatedHits = [...oldHits, ...hits]
-  this.setState({
+  return {
     results: {
       ...results,
       [searchKey]: { hits: updatedHits, page }
     },
     isLoading: false
-  })
+  }
 }
 
 
